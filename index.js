6fd59399c8a5d2228643cd9e2f83f0c252431625
Travis Warlick <twarlick@maxmedia.com>
2022-01-06 18:59:13 -0500

    Fix base directory and update rules

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 module.exports = {
+  configBasedir: 'node_modules',
   extends: 'stylelint-config-standard',
   plugins: [
     'stylelint-scss'
   ],
   rules: {
-    'at-rule-empty-line-before': 'always',
+    'at-rule-empty-line-before': null,
+    // 'at-rule-empty-line-before': [ 'always', { except: 'after-same-name' } ],
+
     'at-rule-no-unknown': null,
-    'scss/at-rule-no-unknown': true
+    'scss/at-rule-no-unknown': true,
+    'no-descending-specificity': [ true, { severity: 'warning' } ],
+    'color-hex-case': null,
+
+    'selector-list-comma-newline-after': null,
+    'no-empty-source': [ true, { severity: 'warning' } ]
   }
 };
